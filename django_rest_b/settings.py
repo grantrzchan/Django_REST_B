039e958f4e2f351a0534acc4abebbed250d0b608
Grant Chan <grant.r.chan@gmail.com>
2020-04-30 00:58:57 -0500

    configured settings.py to use UserProfile

--- django_rest_b/settings.py
+++ django_rest_b/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Set and use custom user model for authentication and user registration
+AUTH_USER_MODEL = 'django_rest_api_b.UserProfile'
