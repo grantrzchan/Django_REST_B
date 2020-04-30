595041da67bb9947f0351c171ff688a33450811d
Grant Chan <grant.r.chan@gmail.com>
2020-04-30 00:55:41 -0500

    created UserModelManager class, user and superuser methods

--- django_rest_api_b/models.py
+++ django_rest_api_b/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionMixin
+from django.contrib.auth.models import BaseUserManager
 
+
+class UserProfileManager(BaseUserManager):
+    '''Manager for user profile'''
+    def create_user(self, email, name, password=None):
+        '''Create a new user profile'''
+        if not email:
+            raise ValueError('User must have an email address')
+        #convert 2nd half email addresss to all lower case and make it case insensitive
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        #encrypt password as a hash
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        '''Create and save a new superuser with given details'''
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 class UserProfile(AbstractBaseUser, PermissionMixin):
     """Database model for users in the system"""
@@ -10,6 +33,7 @@ class UserProfile(AbstractBaseUser, PermissionMixin):
     #creat name column in UserProfile database table
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+    #PermissionMixin creates is_superuser, we need to define is_staff
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
@@ -29,3 +53,4 @@ class UserProfile(AbstractBaseUser, PermissionMixin):
     def __str__(self):
         """Return string representation of user"""
         return self.email
+
